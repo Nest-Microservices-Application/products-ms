94ed81ae78cbfffdd2e83dab78632786e4650571
Martín Cruz <martincruz1426@gmail.com>
2024-07-20 16:53:27 -0300

    Replacing Exception Handling with RpcExceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,10 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from '@prisma/client';
+
 import { PaginationDto } from '../shared';
 
 @Injectable()
@@ -52,7 +50,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product)
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
 
     return product;
   }
